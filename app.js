93dc111710369f8b752d2d253599fca1421368b8
Shane Bell <shanebell@siu.edu>
2018-03-13 01:26:56 +0000

    initial app.js creation, & npm init

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("expresss");
+var app = express();
+
+
+
+app.get("/", function(req, res){
+    res.send("root page");
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("motofotos server has started");
+});
+
