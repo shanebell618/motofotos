b43576c8f3aaad7518f300e958d038345bd32b9a
Shane Bell <shanebell@siu.edu>
2018-03-14 02:53:38 +0000

    add header and footer partials add bootstrap setup photos POST route add body-parser setup route to show new photo form add basic form basic styling of photos page

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//TEMP ARRAY
+var photos = [
+    {name: "Jeff", image: "http://via.placeholder.com/200x200"},
+    {name: "Andrew", image: "http://via.placeholder.com/200x200"},
+    {name: "Steve", image: "http://via.placeholder.com/200x200"}
+];
+//TEMP ARRAY
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/photos", function(req, res){
-    var photos = [
-        {name: "Jeff", image: "https://pixabay.com/get/ea37b30f2cf7003ed1584d05fb1d4e97e07ee3d21cac104497f0c879a2e5b3bf_340.jpg"},
-        {name: "Andrew", image: "https://cdn.pixabay.com/photo/2018/02/16/17/07/motocross-3158085_960_720.jpg"},
-        {name: "Steve", image: "https://cdn.pixabay.com/photo/2018/03/06/20/45/bike-3204546_960_720.jpg"}
-    ]
-    
     res.render("photos", {photos: photos});
 });
 
+app.post("/photos", function(req, res){
+    //get data from form and add to photos
+    var name = req.body.name;
+    var image = req.body.image;
+    var newPhoto = {name: name, image: image};
+    photos.push(newPhoto);
+    
+    //redirect back to photos page
+    res.redirect("/photos");
+});
 
+app.get("/photos/new", function(req, res) {
+   res.render("new");
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
